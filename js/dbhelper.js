f1858b6192561f3198647bd3f18b1901ddd6e5e5
evanh <evanhumanist@gmail.com>
2018-10-22 21:02:12 -0400

    feature: Implemented alt text for all images

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
      */
     static get DATABASE_URL() {
         const port = 8000; // Change this to your server port
-        return `http://localhost:${port}/data/restaurants.json`;
+        return `http://aviserver1:${port}/data/restaurants.json`;
     }
 
     /**
@@ -151,6 +151,10 @@ class DBHelper {
      */
     static imageUrlForRestaurant(restaurant) {
         return (`/img/${restaurant.photograph}`);
+    }
+
+    static altTextForRestaurant(restaurant) {
+        return (restaurant.alt_text)
     }
 
     /**
